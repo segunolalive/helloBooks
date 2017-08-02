c5fd0301a1cdffe7923ef6471e513d5c27a3b09d
segun ola <segunlalive@gmail.com>
2017-08-02 13:26:49 +0100

    setup eslint, sequelize, add dotenv

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+module.exports = app;
---
+++ server/bin/www.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('../app');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
