3fabedc1af1b70af9328f4b41d6e050495b78ded
segun ola <segunlalive@gmail.com>
2017-08-06 13:24:24 +0100

    add api routes

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+import userController from '../controllers/users';
+import bookController from '../controllers/books';
+
+import authenticate from '../middleware/authentication';
+
+
+export default (app) => {
+  app.post('/api/v1/users/signup', userController.createUser);
+  app.post('/api/v1/users/signin', userController.getUser);
+  app.get('/api/v1/books', bookController.getAllBooks);
+  app.get('/api/v1/books/:id', bookController.getBook);
+  app.use(authenticate);
+  app.post('/api/v1/books', bookController.createBook);
+  app.put('/api/v1/books/:id', bookController.editBookInfo);
+};
