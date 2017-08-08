ace5d059133409f71d2453c31f2a0e0d0a463464
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-08-08 14:02:04 +0100

    add  borrowBook, returnBook methods, update editBook method and make api response shape consistent

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,24 +1,42 @@
-import { Book } from '../models';
+import { Book, BorrowedBook } from '../models';
 
 
 export default {
   createBook(req, res) {
-    return Book
-      .create(req.body)
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
+    if (req.user && req.user.isAdmin) {
+      return Book
+        .create(req.body)
+        .then(user => res.status(201).send({
+          success: false,
+          user,
+        }))
+        .catch(error => res.status(400).send({
+          success: false,
+          error
+        }));
+    }
+    res.status(401).send({
+      success: false,
+      message: 'unauthorized access',
+    });
   },
   getBook(req, res) {
     const id = Number(req.params.id);
     Book.findById(id)
       .then((book) => {
         if (!book) {
-          res.send('book does not exist');
+          res.status(400).send('book does not exist');
           return;
         }
-        res.send(book);
+        res.status(200).send({
+          success: true,
+          data: book,
+        });
       })
-      .catch(error => res.status(400).send(error));
+      .catch(error => res.status(400).send({
+        success: false,
+        error
+      }));
   },
   getAllBooks(req, res) {
     Book.findAll()
@@ -29,21 +47,101 @@ export default {
         }
         res.send(books);
       })
-      .catch((error) => {
-        res.status(400).send(error);
-      });
+      .catch(error => res.status(400).send({
+        success: false,
+        error
+      }));
   },
   editBookInfo(req, res) {
     const id = req.params.id;
-    Book.update(
-      req.body,
-      { where: { id },
-        returning: true,
-        plain: true,
-      })
-      .then(book => res.send(book))
-      .catch((error) => {
-        res.status(400).send(error);
+    if (req.user && req.user.isAdmin) {
+      Book.update(
+        req.body,
+        { where: { id },
+          returning: true,
+          plain: true,
+        })
+        .then(book => res.status(200).send({
+          success: true,
+          data: book,
+        }))
+        .catch(error => res.status(400).send({
+          success: false,
+          error
+        }));
+    } else {
+      res.status(401).send({
+        success: false,
+        message: 'unauthorized access',
       });
+    }
+  },
+  borrowBook(req, res) {
+    const userId = req.params.id;
+    const bookId = req.query.id;
+    Book.findById(bookId)
+      .then((book) => {
+        if (!book) {
+          res.status(404).send({
+            success: false,
+            message: 'Book not found',
+          });
+          return;
+        }
+        if (book.total <= 0) {
+          res.status(404).send({
+            success: false,
+            message: 'There are no available copies of this book at this time',
+          });
+          return;
+        }
+        BorrowedBook.findOne({ where: { userId, bookId, returned: false } })
+          .then((borrowed) => {
+            if (borrowed) {
+              res.status(403).send({
+                success: false,
+                message: 'You currently have this book. Return it before' +
+                         'trying to borrow it again',
+              });
+              return;
+            }
+            BorrowedBook.create({
+              userId, bookId,
+            })
+              .then(() => res.status(201).send({
+                success: true,
+                message: 'You have successfully borrowed this book' +
+                         'check your profile to read read it',
+              }));
+          });
+      })
+      .catch(error => res.status(500).res.send({
+        success: false,
+        error,
+      }));
+  },
+  returnBook(req, res) {
+    const bookId = req.query.id;
+    const userId = req.params.id;
+    BorrowedBook.findOne({ where: { userId, bookId, returned: false } })
+      .then((borrowedBook) => {
+        if (borrowedBook) {
+          BorrowedBook.update({
+            returned: true,
+          }, {
+            where: { userId, bookId, returned: false }
+          }).then(() => {
+            res.status(201).send({
+              success: true,
+              message: 'You have successfully returned book',
+            });
+          });
+        }
+      })
+      .catch(error => res.status().send({
+        success: false,
+        error,
+      }));
   }
 };
+
