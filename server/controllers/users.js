f12ac66cbb475da13736cea143fa791d3bb75b97
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-08-08 14:14:06 +0100

    edit creteUser and getUser methods. Rewrite getProfile method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 
-import { User } from '../models';
+import { User, Book } from '../models';
 import { getJWT, hashPassword } from '../helpers/helpers';
 
 dotenv.config();
@@ -9,55 +9,84 @@ dotenv.config();
 
 export default {
   createUser(req, res) {
-    return User.findOne({
-      where: { username: req.body.username }
+    const username = req.body.username;
+    const email = req.body.email;
+    return User.find({
+      where: { $or: [{ username }, { email }] }
     }).then((existingUser) => {
-      if (existingUser) {
-        res.send('username is taken');
+      if (existingUser && existingUser.username === username) {
+        res.status(400).json({
+          success: false,
+          message: 'username is taken',
+        });
+        return;
+      }
+      if (existingUser && existingUser.email === email) {
+        res.status(400).json({
+          success: false,
+          message: 'email is associated with an account',
+        });
         return;
       }
       User.create(req.body)
         .then(user => hashPassword(user))
         .then(user => user.save())
-        .then(user => res.status(201).send(user))
-        .catch(error => res.status(400).send(error));
-    });
+        .then(() => res.status(201).send({
+          success: true,
+          message: 'account created',
+        }))
+        .catch(error => res.status(400).send({
+          success: false,
+          error
+        }));
+    })
+      .catch(error => res.status(500).send({
+        success: false,
+        error
+      }));
   },
   getUser(req, res) {
     const username = req.body.username;
     const password = req.body.password;
     return User.findOne({ where: { username } }).then((user) => {
       if (!user) {
-        res.send('user does not exist');
+        res.staus(400).send({
+          success: false,
+          message: 'user does not exist',
+        });
         return;
       }
       bcrypt.compare(password, user.password).then((result) => {
         if (!result) {
-          res.send('wrong username and password combination');
+          res.status(400).send({
+            success: false,
+            message: 'wrong username and password combination',
+          });
         } else {
           const token = getJWT(user.id, user.username, user.email);
-          res.status(200).send(token);
+          res.status(200).json({ success: true, token });
         }
       });
-    });
-  },
-  borrowBook(req, res) {
-    const userId = req.params.id;
-    const bookId = req.body.id;
-    res.send(userId);
-  },
-  returnBook(req, res) {
-    const userId = req.params.id;
-    const bookId = req.body.id;
-    res.send(userId);
+    }).catch(error => res.status(400).send({
+      success: false,
+      error,
+    }));
   },
   getProfile(req, res) {
-    // const returned;
     const id = req.params.id;
-    User.findAll({
+    User.findOne({
       where: { id },
-      include: [{
-        model: 'Book', }]
-    }).then(books => res.status(200).send(books));
+      include: [{ model: Book }]
+    }).then((user) => {
+      const books = user.Books.filter(
+        book => book.BorrowedBook.returned === true
+      );
+      res.status(200).send(books);
+    })
+      .catch(error => res.status(500).send({
+        success: false,
+        message: 'unable to fetch profile info',
+        error,
+      }));
   },
 };
