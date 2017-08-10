fec4a93876cc74572a6bda69f76f5a7ee5b16738
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2017-08-10 05:41:43 +0100

    make new schema migrations

---
+++ server/migrations/20170809183304-create migration for user table.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      isAdmin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  down(queryInterface /* , Sequelize */) {
+    return queryInterface.dropTable('Users');
+  }
+};
