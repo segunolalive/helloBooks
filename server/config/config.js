e009a24b459b315a7f6560eea70ab99c8ae5bcac
segun ola <segunolalive@gmail.com>
2017-08-11 07:42:49 +0100

    remove comments

--- server/config/config.js
+++ server/config/config.js
@@ -15,14 +15,6 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    // username: 'bqqcndgg',
-    // password: 'sFziUyi8b-5TV3B3KX8iAVTTn81PWTXJ',
-    // database: 'bqqcndgg',
-    // dialect: 'postgres',
-    // host: 'babar.elephantsql.com'
   }
-  // production: {
-  //   use_env_variable: 'DATABASE_URL',
-  //   dialect: 'postgres',
-  // }
 };
+
