5b2941a943e004543f3e5788fcc348733d5361dd
segun ola <segunlalive@gmail.com>
2017-08-26 06:16:17 +0100

    add logo component

---
+++ client/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Logo = () => (
+  <a href='#!' className='brand-logo left'>
+    <i className="material-icons">
+      book
+    </i>
+    Hello Books
+  </a>
+);
+
+export default Logo;
