f0cf165e2461e766f51f6af2ae5a84b14cca2a13
segun ola <segunlalive@gmail.com>
2017-08-26 06:18:07 +0100

    add landing page component

---
+++ client/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="row">
+    <div className="container">
+      <section className="center landing">
+        <div>
+          <h1>Welcome to Hello Books</h1>
+        </div>
+        <div>
+          <a href="./login" className="btn">login</a>
+          <a href="./signup" className="btn">signup</a>
+        </div>
+      </section>
+    </div>
+  </div>
+);
+
+export default Home;
