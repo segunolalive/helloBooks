e6545305bd46f71ac0cb98943b0fd9c3ace1d093
segun ola <segunlalive@gmail.com>
2017-08-26 05:59:10 +0100

    set up webpack and babel for react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,56 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 
 module.exports = {
-  entry: './server/app.js',
+  entry: './client/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/server')
-  }
+    path: path.resolve(__dirname, 'dist/client')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: ['node_modules', 'server', 'test', 'dist'],
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: ['node_modules', 'dist'],
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node-modules/,
+        loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|png|jpg|gif)$/,
+        loader: 'url-loader?limit=250000'
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig],
 };
