976e0731314fd22d3bee821a9f469fc6abb06a5f
segun ola <segunlalive@gmail.com>
2017-08-30 22:49:33 +0100

    Add Login action creator

---
+++ client/actions/login.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import actionTypes from '../actions/actionTypes';
+
+
+export const loginUser = (user => ({
+  type: actionTypes.LOGIN,
+  user,
+}));
+
+// export const API = 'https://segunolalive-hellobooks.herokuapp.com/api/v1';
+const API = 'https://hellobooks.herokuapp.com/api/v1';
+
+export /**
+ * @param {any} data - user data
+ * @returns {any} - dispatches login user action
+ */
+
+// const login = data => axios.post(`${API}/login`, data)
+//   .then((response) => {
+//     sessionStorage.setItem('token', response.data.token);
+//     console.log(response.data);
+//   }).catch(error => console.log(error));
+
+const login = data => dispatch => axios.post(`${API}/login`, data)
+  .then((response) => {
+    sessionStorage.setItem('token', response.data.token);
+    console.log(response);
+    dispatch(loginUser(response.data));
+  }, (err) => {
+    console.log(err);
+  })
