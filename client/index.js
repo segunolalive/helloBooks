795d345b7c3ddc6f62062dda1b3a64c55299fb15
segun ola <segunlalive@gmail.com>
2017-08-30 22:55:31 +0100

    Wrap app in Provider component

--- client/index.js
+++ client/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './components/App';
 import './static/index.scss';
 // import registerServiceWorker from './registerServiceWorker';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 // registerServiceWorker();
