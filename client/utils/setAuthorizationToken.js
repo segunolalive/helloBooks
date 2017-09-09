8ac23534a92fd9ab0876f8b8814a1dac1255b905
Segun Ola <segunolalive@gmail.com>
2017-09-09 08:53:48 +0100

    add authentication header to axios requests

---
+++ client/utils/setAuthorizationToken.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const setAuthorizationToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['x-access-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-access-token'];
+  }
+};
+
+export default setAuthorizationToken;
