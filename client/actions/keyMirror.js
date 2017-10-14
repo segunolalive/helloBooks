50f379d7a26ecdec65a5b2327d54f7c5465dda13
Segun Ola <segunolalive@gmail.com>
2017-10-14 15:18:45 +0100

    chore(jsdocs): addmjsdocs to miscellaneous files in client

--- client/actions/keyMirror.js
+++ client/actions/keyMirror.js
@@ -1,3 +1,9 @@
+/**
+ * converts an array to Object with array items as keys and values
+ * @example ['books'] becomes { books: 'books' }
+ * @param  {Array} keys  array of keys
+ * @return {Object}      Object with keys and values mirrored
+ */
 export default (keys) => {
   keys = Array.isArray(keys) ? keys : Object.keys(keys);
   const mirror = {};
--- client/utils/setAuthorizationToken.js
+++ client/utils/setAuthorizationToken.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+/**
+ * set or remove authorization token on request headers
+ * @param {String} token authorization
+ * @returns {Undefined} sets token on request header
+ */
 const setAuthorizationToken = (token) => {
   if (token) {
     axios.defaults.headers.common['x-access-token'] = token;
