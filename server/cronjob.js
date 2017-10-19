3ac6ba5338d4dac41a30d4706c5049b91ccf888b
Segun Ola <segunolalive@gmail.com>
2017-10-19 07:41:31 +0100

    feature(defaulter emails): mail users who do not returned as when due

--- server/cronjob.js
+++ server/cronjob.js
@@ -1,27 +1,43 @@
-import moment from 'moment';
+import sequelize from 'sequelize';
 import { BorrowedBook, User } from './models';
 import { borrowingDuration } from './helpers/borrowingLimits';
 import { transporter, mailOptions } from './config/mail';
 
+const Op = sequelize.Op;
+const timeLimit = borrowingDuration * 1000 * 60 * 60 * 24;
+
 const defaulters = () => {
-  BorrowedBook.find({
-    where: { returned: false }
+  BorrowedBook.findAll({
+    where: {
+      returned: false,
+      updatedAt: {
+        [Op.lt]: new Date(new Date() - timeLimit)
+      }
+    },
+    attributes: ['userId']
   })
-    .then((rows) => {
-      const defaultersList = rows.filter((row) => {
-        const timeBorrowed = new Date(row.createdAt);
-        const daysElapsed = Date.now() - timeBorrowed;
-        return daysElapsed >= borrowingDuration * 1000 * 60 * 60 * 24;
-      });
-      return defaultersList;
+    .then((borrowedBooks) => {
+      const ids = borrowedBooks.map(book => book.userId);
+      User.findAll({
+        where: {
+          id: { [Op.in]: ids }
+        },
+        attributes: ['email']
+      })
+        .then((users) => {
+          const emails = users.map(user => user.email);
+          const to = emails;
+          const bcc = null;
+          const subject = 'Defaut on Returning Book';
+          const html = '<h3>Hello. This is to notify you that you have' +
+           'exceeded the borrowing duration for one of our books and would' +
+           'start getting billed for the book.</h3>';
+          transporter.sendMail(mailOptions(to, bcc, subject, html));
+        }, error => process.stdout.write(error.stack));
+    }).catch((error) => {
+      process.stdout.write(error.stack);
+      process.exit(0);
     });
 };
 
-const defaulterEmails = defaulters.map((defaulter) => {
-  User.find
-})
-
-const notifyDefaulters = () => {
-  const receipients = defaulters();
-  transporter.sendMail(mailOptions())
-};
+defaulters();
