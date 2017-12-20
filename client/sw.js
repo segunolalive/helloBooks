332d6b874020d248705fbde621e79b68aed9e6b4
Segun <segunolalive@gmail.com>
2017-12-20 11:46:26 +0100

    feature(read-book): make app offline-first

--- client/sw.js
+++ client/sw.js
@@ -1,48 +1,45 @@
-// In production, we register a service worker to serve assets from local cache.
+importScripts('workbox-sw.prod.js');
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since previously
-// cached resources are updated in the background.
 
-// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes instructions on opting out of this behavior.
+const workbox = new WorkboxSW({
+  skipWaiting: true,
+  clientsClaim: true
+});
 
-export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      navigator.serviceWorker.register(swUrl).then(registration => {
-        registration.onupdatefound = () => {
-          const installingWorker = registration.installing;
-          installingWorker.onstatechange = () => {
-            if (installingWorker.state === 'installed') {
-              if (navigator.serviceWorker.controller) {
-                // At this point, the old content will have been purged and
-                // the fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in your web app.
-                console.log('New content is available; please refresh.');
-              } else {
-                // At this point, everything has been precached.
-                // It's the perfect time to display a
-                // "Content is cached for offline use." message.
-                console.log('Content is cached for offline use.');
-              }
-            }
-          };
-        };
-      }).catch(error => {
-        console.error('Error during service worker registration:', error);
-      });
-    });
-  }
-}
 
-export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
-      registration.unregister();
-    });
-  }
-}
+workbox.router.registerRoute(
+  /^https:\/\/res\.cloudinary\.com\/hfdd5itnd\/image\/upload\//,
+  workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.router.registerRoute('/api/v1/', workbox.strategies.networkFirst());
+workbox.router.registerRoute('/api-docs/', workbox.strategies.networkFirst());
+
+
+self.addEventListener('push', (event) => {
+  const title = 'Get started with Hello Books';
+  const options = {
+    body: event.data.text()
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open('dynamic')
+      .then(cache => (
+        cache.match(event.request)
+          .then((response) => {
+            const fetchPromise = fetch(event.request)
+              .then((networkResponse) => {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              });
+            return response || fetchPromise;
+          })
+      ))
+  );
+});
+
+
+workbox.precache([]);
