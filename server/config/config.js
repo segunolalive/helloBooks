0ed8f1f4c3e890b7e1e1c424afda6134b71c6f51
Segun <segunolalive@gmail.com>
2018-01-03 06:15:46 +0100

    chore(feedback): amend test db url

--- server/config/config.js
+++ server/config/config.js
@@ -10,15 +10,8 @@ module.exports = {
     host: process.env.host,
     dialect: process.env.dialect,
   },
-  // test: {
-  //   use_env_variable: 'DATABASE_URL_TEST',
-  // },
   test: {
-    username: process.env.test_username,
-    password: process.env.test_password,
-    database: process.env.test_database,
-    host: process.env.localhost,
-    dialect: process.env.dialect,
+    use_env_variable: 'DATABASE_URL_TEST',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
